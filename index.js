971b0662a708d2293507c72389f3f50e25e754d2
Yukti Gupta <yuktigupta95@gmail.com>
2026-07-13 14:30:02 +0530

    Remove challenge comments and streamline fetch implementation for blog posts

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-/**
- Challenge:
- 
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
- 
- Log the 5 items to the console
- */
+const baseURL = "https://apis.scrimba.com/jsonplaceholder";
 
+fetch(`${baseURL}/posts`, { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => console.log(data.slice(0, 5)));
